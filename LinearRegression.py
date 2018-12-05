bd1de3a8f6e3a41f119c292e0552ff5d4d17a4f2
euijun0109 <39749842+euijun0109@users.noreply.github.com>
2018-12-04 19:50:56 -0500

    Add files via upload

---
+++ LinearRegression.py
@@ -0,0 +1,50 @@
+import matplotlib.pyplot as plt
+class LinearRegression:
+    def __init__(self, m, alpha):
+        self.theta1 = 0
+        self.theta2 = 0
+        self.m = m
+        self.cost1 = 0
+        self.cost2 = 0
+        self.alpha = alpha
+
+    def hypothesis(self, x):
+        return self.theta1 + self.theta2 * x
+
+    def batch(self, pair):
+        for x, y in pair:
+            self.cost1 += self.hypothesis(x) - y
+            self.cost2 += (self.hypothesis(x) - y) * x
+
+    def GD(self):
+        newTheta1 = self.theta1 - self.alpha * (1/self.m) * (self.cost1)
+        newTheta2 = self.theta2 - self.alpha * (1/self.m) * (self.cost2)
+        self.theta1 = newTheta1
+        self.theta2 = newTheta2
+
+    def calculate(self, *train):
+        for pair in train:
+            self.batch(pair)
+        self.GD()
+        print("costs:", self.cost1, ", ", self.cost2, '\n')
+        self.cost1 = 0
+        self.cost2 = 0
+        print("theta 1: ", self.theta1, "theta 2: ", self.theta2, '\n')
+
+    def run(self, i, train):
+        for _ in range(i):
+            self.calculate(train)
+        self.display(train)
+
+    def display(self, data):
+        x = []
+        y = []
+        for pair in data:
+            x.append(pair[0])
+            y.append(pair[1])
+        X = [0, x[self.m - 1] + 1]
+        Y = [self.theta1 + self.theta2 * 0, self.theta1 + self.theta2 * (x[self.m - 1] + 1)]
+        plt.plot(x, y, "ro", label="data")
+        plt.plot(X, Y, label="best fit line")
+        plt.legend()
+        plt.show()
---
+++ LinearRegressionTest.py
@@ -0,0 +1,6 @@
+import LinearRegression as lr
+
+train = [(1, 3), (2, 6), (3, 7), (6, 9), (4, 8), (5, 6)]
+LR = lr.LinearRegression(6, 0.1)
+LR.run(10000, train)
+
